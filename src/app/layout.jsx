6c0b7113ea971717292fbf83ad44cfb93d77bd34
Meheady <hmmehedi55@gmail.com>
2025-10-11 19:45:47 +0600

    pwa for mobile

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -21,6 +21,21 @@ export default function RootLayout({ children }) {
         <html lang="en">
         <head>
             <link rel="manifest" href="/manifest.json" />
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: `
+                        if ('serviceWorker' in navigator) {
+                            window.addEventListener('load', () => {
+                                navigator.serviceWorker.register('/sw.js').then(registration => {
+                                    console.log('SW registered: ', registration);
+                                }).catch(registrationError => {
+                                    console.log('SW registration failed: ', registrationError);
+                                });
+                            });
+                        }
+                    `,
+                }}
+            />
         </head>
         <body className={roboto.className}>
 
