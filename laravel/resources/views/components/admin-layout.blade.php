91153d3dd801f51530c253204a6e8af2bc7d1331
Meheady <64876349+Meheady@users.noreply.github.com>
2026-01-12 15:01:20 +0600

    fix design

--- laravel/resources/views/components/admin-layout.blade.php
+++ laravel/resources/views/components/admin-layout.blade.php
@@ -12,7 +12,9 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- Scripts -->
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
     <style>
         .admin-layout {
             display: flex;
